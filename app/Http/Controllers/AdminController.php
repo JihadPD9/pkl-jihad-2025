dfe296a5a27917bb69a20c3e1c627fba9f10f1de
Jihad <muhammadjihad1099@gmail.com>
2025-12-17 11:50:17 +0700

    day 3 part 3

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function dashboard()
+    {
+        return view('admin.dashboard');
+    }
+    
     /**
      * Display a listing of the resource.
      */
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Sepatu Keren</title>
+</head>
+<body>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <center>
+    <h1>Ini Halaman Admin</h1>
+    </center>
+</body>
+</html>
